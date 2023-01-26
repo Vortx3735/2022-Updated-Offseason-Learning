2eb7e9e8bde9589489393fa1d7c41acdccef3cf8
Udunen <udunenk@gmail.com>
2023-01-25 18:54:14 -0600

    OCD changes
    Removed some unneeded imports in IndexerSub.java, and fixed the annoying warning in ExampleCommand.java

--- src/main/java/frc/robot/subsystems/IndexerSub.java
+++ src/main/java/frc/robot/subsystems/IndexerSub.java
@@ -5,9 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-//import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-//import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
